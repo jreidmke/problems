4f304f40220136ee21ab707bc122e4e5ed54d7c2
James Reid <jreidmke@gmail.com>
2021-03-26 10:43:39 -0500

    vanilla binary search

--- app.js
+++ app.js
@@ -351,3 +351,18 @@ function findMinRotated(arr) {
         lastEl = arr[right];
     } 
 }
+
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+    while(left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if(arr[mid] > target) right = mid - 1;
+
+        if(arr[mid] < target) left = mid + 1;
+
+        if(arr[mid] === target) return mid;
+    }
+    return -1
+}
